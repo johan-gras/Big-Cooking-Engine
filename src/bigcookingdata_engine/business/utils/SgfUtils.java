0c9948241d9fc3bc4057a077fa5a994e9f080d5d
SofianeH1 <sofiane.hamiti18@gmail.com>
2018-03-06 19:38:02 +0100

    outils pour gestion de fichiers

---
+++ src/bigcookingdata_engine/business/utils/SgfUtils.java
@@ -0,0 +1,94 @@
+package sgf;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author sofiane-hamiti
+ *
+ */
+public class SgfUtils {
+
+	/**
+	 * Method pour créer un fichier txt
+	 * 
+	 * @param name
+	 *            le nom du ficher
+	 */
+
+	public String readLine(int line) {
+
+		try {
+
+			return Files.readAllLines(Paths.get("data/da")).get(0);
+
+		} catch (IOException e) {
+
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Ecrire dans une ligne bien spécifique
+	 * 
+	 * @param line
+	 *            numero de la ligne (id uer)
+	 * @param value
+	 *            la valeur
+	 */
+	public void writeLine(int line, String value) {
+
+		List<String> lines = null;
+
+		File f = new File("data/da");
+
+		try {
+
+			lines = Files.readAllLines(f.toPath());
+
+			lines.set(line, value);
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+	}
+
+	/**
+	 * Methode pour initialiser le veteur à 0
+	 * 
+	 * @param name
+	 *            nom du fichier
+	 */
+	public void initFile(String name) {
+
+		List<String> zero = new ArrayList<String>();
+
+		for (int i = 1; i < 1312; i++) {
+
+			zero.add("0");
+
+		}
+
+		try {
+
+			Files.write(Paths.get("data/" + name + ".txt"), zero, Charset.forName("UTF-8"));
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+}
