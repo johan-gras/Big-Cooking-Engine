f5b507d7baf003cbc92170591cae43041ac6b411
johan.gras <johan.gras-sub@outlook.com>
2018-06-05 14:24:36 +0200

    My bad mini-fix constructor

--- src/bigcookingdata_engine/controller/ServletSearch.java
+++ src/bigcookingdata_engine/controller/ServletSearch.java
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import bigcookingdata_engine.business.data.recipe.Ingredient;
 import bigcookingdata_engine.business.data.recipe.Recipe;
 import bigcookingdata_engine.database.SerchEngin;;
 
@@ -27,10 +29,9 @@ public class ServletSearch extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		SerchEngin se = new SerchEngin();
-		int[] ing = new int[2];
 		ArrayList<Recipe> recipeliste = new ArrayList<>();
 		Recipe r = new Recipe( "timeTotal", "categorie", 7, 7, "timeCooking", 8,
-                "timePrepa", "title", 7, 7, ing,7 );
+                "timePrepa", "title", 7, 7 ,7, null, null, null );
 		recipeliste.add(r);
 		recipeliste = se.getRecipe("fromage");
 		System.out.println("nb recettes :"+recipeliste.size());
@@ -45,7 +46,7 @@ public class ServletSearch extends HttpServlet {
 		int[] ing = new int[2];
 
 		Recipe r = new Recipe( "timeTotal", "categorie", 7, 7, "timeCooking", 8,
-                "timePrepa", "title", 7, 7, ing,7 );
+                "timePrepa", "title", 7, 7,7, null,null, null );
 		
 		//on affécte l'objet pour le requete
 		request.setAttribute("recette", r);
